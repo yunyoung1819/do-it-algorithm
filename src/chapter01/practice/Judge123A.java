4e02262d05dae82723be9d51069d396349dcbf30
yunyoung1819 <yunyoung1819@hanmail.net>
2022-01-24 01:10:59 +0900

    [ADD] chapter3장 선형검색 실습

---
+++ src/chapter01/practice/Judge123A.java
@@ -0,0 +1,19 @@
+package chapter01.practice;
+
+import java.util.Scanner;
+
+public class Judge123A {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.print("정수를 입력하세요. : ");
+        int n = stdIn.nextInt();
+
+        if (n == 1)
+            System.out.println("이 수는 1입니다.");
+        else if (n == 2)
+            System.out.println("이 수는 2입니다.");
+        else if (n == 3)
+            System.out.println("이 수는 3입니다.");
+    }
+}
